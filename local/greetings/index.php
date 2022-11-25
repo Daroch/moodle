bcc4e60af554a04ae7067be8a91bea41ab860fbe
Daroch <daroch314@gmail.com>
2022-11-25 14:03:56 +0100

    permisos añadidos para ocultar link del menu a usuarios invitados

--- local/greetings/index.php
+++ local/greetings/index.php
@@ -31,6 +31,11 @@ $PAGE->set_pagelayout('standard');
 $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
+require_login();
+if (isguestuser()) {
+    throw new moodle_exception('noguest');
+}
+
 $messageform = new local_greetings_message_form();
 
 if ($data = $messageform->get_data()) {
